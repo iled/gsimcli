5e1b558f83ec71d8e5ebf600d73f10b57ecf8d0b
Júlio <drawzal@yahoo.com>
2015-02-01 07:37:47 +0000

    fix download and install sequence

--- GSIMCLI/interface/install_dataset.py
+++ GSIMCLI/interface/install_dataset.py
@@ -4,23 +4,23 @@ Created on 31/01/2015
 
 @author: julio
 """
-from PySide import QtCore, QtGui
 import hashlib
 import os
 import sys
 import tempfile
-import time
 import urllib2
 from zipfile import ZipFile
 
-base = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(base)
+from PySide import QtGui
 
 from external_libs.pyside_dynamic import loadUi
 from interface.ui_utils import Office, Updater, hide
 
 
-class InstallDialog(QtGui.QWidget):
+base = os.path.dirname(os.path.dirname(__file__))
+
+
+class InstallDialog(QtGui.QDialog):
     """Dialog to the user download and/or install the benchmark data set.
 
     """
@@ -34,74 +34,101 @@ class InstallDialog(QtGui.QWidget):
         super(InstallDialog, self).__init__(parent)
         # load ui file
         loadUi(os.path.join(base, "interface", "install_dataset.ui"), self)
-        self.downloading = False
-        self.installing = False
+        self.temp_file = os.path.join(tempfile.gettempdir(), 'benchmark.zip')
+        self.benchmark_path = None
+        
+        if hasattr(self.parent, "default_dir"):
+            self.default_dir = self.parent.default_dir
+        else:
+            self.default_dir = os.path.expanduser('~/')
+            
+        # action
+        button_ok = self.buttonBox.button(QtGui.QDialogButtonBox.Ok)
+        button_ok.clicked.connect(self.accept)
+        button_cancel = self.buttonBox.button(QtGui.QDialogButtonBox.Cancel)
+        button_cancel.clicked.connect(self.close)
 
         # buttons
+        self.buttonDatasetPath.clicked.connect(self.browse_path)
         self.buttonDownload.clicked.connect(self.start_download)
 
         # hide
         hide([self.progressBar, self.labelStatus])
 
-    @QtCore.Slot()
     def accept(self):
-        print "yep"
+        user_path = self.lineDatasetPath.text()
+        if hasattr(self, "path"):
+            self.benchmark_path = self.path
+        elif user_path and os.path.isdir(user_path):
+            self.benchmark_path = user_path
+        self.done(QtGui.QDialog.Accepted)
+        
+    def browse_path(self):
+        caption = "Select the directory with the benchmark data set"
+        path = QtGui.QFileDialog.getExistingDirectory(self, caption,
+                                                      dir=self.default_dir)
+        if path:
+            self.path = path
+            self.default_dir = os.path.abspath(path)
+            self.lineDatasetPath.setText(path)
 
-    def check_file(self):
-        if os.path.isfile(self.temp_file):
-            hash = hashlib.sha256(open(self.temp_file, 'rb').open())
-            if hash.hexdigest() != self.sha256:
-                pass
-            else:
-                test = ZipFile(self.temp_file).testzip()
+    def check_zip_exists(self):
+        if hasattr(self, "temp_file") and os.path.isfile(self.temp_file):
+            return True
+        else:
+            return False
 
+    def check_zip_hash(self, set_status=False):
+        hashfile = hashlib.sha256(open(self.temp_file, 'rb').read())
+        if hashfile.hexdigest() != self.sha256:
+            status = ("Invalid or corrupted file. Please try to download it "
+                      "again.")
+            ok = False
+        else:
+            status = "File checksum verified."
+            ok = True
+        if set_status:
+            self.set_status(status)
+        return ok
+
+    def check_zip_test(self, set_status=False):
+        test = ZipFile(self.temp_file).testzip()
         if test is None:
-            status = "No errors detected in the downloaded file."
-            time.sleep(2)
+            status = "File integrity verified."
+            ok = True
         else:
             status = "Test failed in the file {}.".format(test)
-        self.labelStatus.setText(status)
+            ok = False
+        if set_status:
+            self.set_status(status)
+        return ok
+    
+    def completed_install(self):
+        self.set_status("Completed")
+        self.show_progress(False)
+        self.lineDatasetPath.setText(self.path)
 
-    def download_(self, file_url=None):
-        self.downloading = True
+    def download(self, file_url=None):
         if file_url is None:
             file_url = ("ftp://ftp.meteo.uni-bonn.de/pub/victor/costhome/"
                         "homogenized_monthly_benchmark/benchmark.zip")
         open_url = urllib2.urlopen(file_url)
         meta = open_url.info()
         self.file_size = int(meta.getheaders('Content-Length')[0])
-        self.temp_file = os.path.join(tempfile.gettempdir(), 'benchmark.zip')
- 
-        downloaded_bytes = 0
         self.update.reset()
         block_size = 1024 * 8
- 
+
         with open(self.temp_file, 'w+b') as f:
             while True:
                 buffering = open_url.read(block_size)
                 if not buffering:
                     break
- 
+
                 f.write(buffering)
-                downloaded_bytes += block_size
                 self.update.current += block_size
                 self.update.send()
-                print downloaded_bytes
-
-    def download(self):
-        self.downloading = True
-        self.update.reset()
-        self.file_size = 3
-        self.temp_file = '/tmp/benchmark.zip'
-        for i in xrange(3):
-            time.sleep(1)
-            self.update.current = i+1
-            self.update.send()
-            print i
-        print "out download"
-        
-    def download_and_install(self):
-        self.check_file()
+        self.set_status("Download completed")
+        self.show_progress(False)
 
     def get_install_path(self):
         user_path = self.lineDatasetPath.text()
@@ -115,10 +142,6 @@ class InstallDialog(QtGui.QWidget):
         return self.path
 
     def install(self):
-        print "installing"
-        self.installing = True
-        self.progressBar.setValue(0)
-        self.labelStatus.setText('Installing...')
         self.update.reset()
         self.get_install_path()
         with ZipFile(self.temp_file) as zipfile:
@@ -128,54 +151,52 @@ class InstallDialog(QtGui.QWidget):
                 zipfile.extract(member, self.path)
                 self.update.current = i
                 self.update.send()
- 
-        self.show_status(False)
-        self.lineDatasetPath.setText(self.path)
-
-    @QtCore.Slot()
-    def reject(self):
-        print "noooo"
 
     def set_progress(self, value):
-        who = "download"#self.sender().job.__name__
-        print who
+        who = self.sender().job.__name__
         if who == "download":
             total = self.file_size
         elif who == "install":
             total = self.zip_items
-        else:
-            print "ops"
-            return
         progress = 100 * value / total
         self.progressBar.setValue(progress)
-        print value, "out set progress"
-        #time.sleep(2)
-        print "pi"
 
-    def show_status(self, toggle):
-        self.progressBar.setValue(0)
-        self.progressBar.setVisible(toggle)
-        self.labelStatus.setVisible(toggle)
-        
-    def start_download(self, **kwargs):
-        if self.downloading:
-            self.labelStatus.setText("File already downloaded. "
-                                     "Checking file integrity.")
+    def set_status(self, status=None):
+        if status is not None:
+            self.labelStatus.setText(status)
             self.labelStatus.setVisible(True)
-            self.check_file()
-            self.install()
         else:
-            self.show_status(True)
+            self.labelStatus.setVisible(False)
+            
+    def show_progress(self, show):
+        self.progressBar.setValue(0)
+        self.progressBar.setVisible(show)
+
+    def start_download(self, **kwargs):
+        download = True
+        if self.check_zip_exists():
+            # FIXME: label not showing up
+            self.set_status("Existing file detected. Checking file...")
+            if self.check_zip_hash() and self.check_zip_test():
+                download = False
+
+        if download:
+            self.set_status("Downloading...")
+            self.show_progress(True)
             self.office = Office(self, self.download, self.update.progress,
                                  **kwargs)
             self.office.worker.update_progress.connect(self.set_progress)
             self.office.finished.connect(self.start_install)
             self.office.start()
+        else:
+            self.start_install()
 
     def start_install(self):
-        print "start install"
+        self.set_status("Installing...")
+        self.show_progress(True)
         self.office = Office(self, self.install, self.update.progress)
         self.office.worker.update_progress.connect(self.set_progress)
+        self.office.finished.connect(self.completed_install)
         self.office.start()
 
 
