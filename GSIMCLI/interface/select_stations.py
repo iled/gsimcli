15eef4fa7803f41fe8c5379a90af0665189d359f
JC <drawzal@yahoo.com>
2015-05-20 19:43:27 +0100

    using the tablemodel/view with a checkbox

--- GSIMCLI/interface/select_stations.py
+++ GSIMCLI/interface/select_stations.py
@@ -4,18 +4,99 @@ Created on 14/05/2015
 
 @author: julio
 """
-from PySide import QtGui
+from PySide import QtGui, QtCore
 import os
 import sys
 
 from external_libs.pyside_dynamic import loadUi
-from interface.ui_utils import pylist_to_qlist
+from interface.checktable import TableModel, TableView
+# from interface.ui_utils import pylist_to_qlist
 import numpy as np
+import pandas as pd
 from tools.grid import PointSet
+from PyQt4.Qt import QPoint
 
 
 base = os.path.dirname(os.path.dirname(__file__))
 
+
+# class TableModel(QtCore.QAbstractTableModel):
+#     """Table model with 3 columns and a variable number of rows. The columns
+#     are : `Station ID` number; `X` coordinate; and `Y` coordinate.
+#
+#     """
+#
+#     def __init__(self, parent=None):
+#         """Constructor.
+#
+#         parent : QObject, optional
+#             Parent of the model.
+#
+#         """
+#         super(TableModel, self).__init__(parent)
+#         self.table = None
+#
+#     def addItem(self, item):
+#         """Append a row to the table.
+#
+#         item : array_like
+#             Row to be inserted after the last row in the table.
+#
+#         """
+#         count = self.rowCount()
+#         self.beginInsertRows(QtCore.QModelIndex(), count, count)
+#         self.table.loc[count] = item
+#         self.endInsertRows()
+#
+#     def columnCount(self, parent=QtCore.QModelIndex()):
+#         """Return the number of columns in the table.
+#
+#         """
+#         if self.table is not None:
+#             return self.table.shape[1]
+#
+#     def data(self, index, role=QtCore.Qt.DisplayRole):
+#         """Return the value in the given index of the table.
+#
+#         """
+#         if role == QtCore.Qt.DisplayRole:
+#             i = index.row()
+#             j = index.column()
+#             return self.table.iloc[i, j]
+#
+#     def get_row(self, row):
+#         """Return the data in the given `row` number.
+#
+#         Parameters
+#         ----------
+#         row : int
+#             Row number
+#
+#         """
+#         return self.table.loc[row]
+#
+#     def setData(self, index, value, role=QtCore.Qt.EditRole):
+#         """Set the `role` data for the item at `index` to `value`.
+#
+#         """
+#         if role == QtCore.Qt.EditRole:
+#             j = index.row()
+#             j = index.column()
+#             self.table.iloc[i, j] = value
+#             self.dataChanged.emit(index, index)
+#             return True
+#         return False
+#
+#
+# class TableView(QtGui.QTableView):
+#     """A table view to show and manage the data in the table model with three
+#     columns.
+#
+#     The table has alternating row colours.
+#
+#     """
+#     def __init__(self, parent=None):
+#         """Constructor
 
 class SelectStations(QtGui.QDialog):
 
@@ -60,6 +141,25 @@ class SelectStations(QtGui.QDialog):
 
         # spin
         self.spinCol.valueChanged.connect(self.refresh)
+
+        # table
+        # model and view
+        self.tableStationsModel = TableModel(3, self, auto_row=False)
+        self.tableStationsView = TableView(3, self)
+        # initialise the table
+        columns = ['Station ID', 'X', 'Y', 'Use']
+        table = pd.DataFrame(columns=columns)
+        self.tableStationsModel.update(table)
+        self.tableStations.close()
+        view = self.tableStationsView
+        view.setModel(self.tableStationsModel)
+        view.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        self.layout().insertWidget(5, self.tableStationsView)
+        self.hheader = self.tableStationsView.horizontalHeader()
+        self.hheader.setResizeMode(0, QtGui.QHeaderView.Stretch)
+        self.hheader.setResizeMode(1, QtGui.QHeaderView.Stretch)
+        self.hheader.setResizeMode(2, QtGui.QHeaderView.Stretch)
+        self.hheader.setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
 
     def accept(self):
         """Custom accept event. Save the selected candidate stations.
@@ -125,9 +225,13 @@ class SelectStations(QtGui.QDialog):
         try:
             self.pset = PointSet()
             self.pset.load(self.path, header=self.header)
-        except ValueError:
-            self.pset = None
-            return None
+        except ValueError, e:
+            # error loading PointSet, perhaps the header information is wrong
+            self.checkHeader.toggle()
+            pos = self.checkHeader.mapToGlobal(QtCore.QPoint(0, 0))
+            text = ("Header should be like this, otherwise an error occurs "
+                    "while loading the PointSet:\n" + unicode(e))
+            QtGui.QToolTip.showText(pos, text, self)
         except AttributeError:
             self.pset = None
             return None
@@ -162,7 +266,7 @@ class SelectStations(QtGui.QDialog):
             # assuming X and Y are the first 2 columns
             locs[station] = st.iloc[0, :2]
         self.locs = locs
-        print locs
+        # print locs
 
     def preview_data_file(self):
         """Set the QPlainTextEdit to preview the first 10 lines of a data file.
@@ -187,6 +291,7 @@ class SelectStations(QtGui.QDialog):
         stations list.
 
         """
+        self.tableStationsModel.clear()
         self.preview_data_file()
         self.load_data()
         self.set_list()
@@ -211,10 +316,13 @@ class SelectStations(QtGui.QDialog):
         Connected to the selectAll checkbox.
 
         """
-        if toggle:
-            self.tableStations.selectAll()
-        else:
-            self.tableStations.clearSelection()
+#         if toggle:
+#             self.tableStations.selectAll()
+#         else:
+#             self.tableStations.clearSelection()
+        for row in range(self.tableStationsModel.rowCount()):
+            index = self.tableStationsModel.index(row, 3)
+            self.tableStationsModel.setData(index, toggle)
 
     def set_list(self):
         """Build the list with the stations' IDs.
@@ -225,23 +333,28 @@ class SelectStations(QtGui.QDialog):
             stations = np.unique(self.pset.values.ix[:, id_col])
             # stations_list = map(int, stations)
         except AttributeError:
+            # the point set was not correctly loaded yet
             pass
         except KeyError:
+            # the ID column must be wrong
             pass
         else:
             # stations_list = map(unicode, stations.astype('int'))
 
             # pylist_to_qlist(stations_list, self.tableStations)
             self.locations(stations)
-            item = QtGui.QTableWidgetItem
+            #item = QtGui.QTableWidgetItem
             for i, station in enumerate(stations):
                 x, y = self.locs[station]
-                self.tableStations.insertRow(i)
-                self.tableStations.setItem(i, 0, item(station))
-                self.tableStations.setItem(i, 1, item(x))
-                self.tableStations.setItem(i, 2, item(y))
+#                 self.tableStations.insertRow(i)
+#                 self.tableStations.setItem(i, 0, item(station))
+#                 self.tableStations.setItem(i, 1, item(x))
+#                 self.tableStations.setItem(i, 2, item(y))
+                item = [station, x, y, False]
+                self.tableStationsModel.addItem(item)
 
-            self.labelFound.setText(unicode(self.tableStations.rowCount()))
+            self.labelFound.setText(
+                unicode(self.tableStationsModel.rowCount()))
 
     def toggle_all(self, cur, prev):
         """Uncheck the selectAll checkbox if any station is unselected.
